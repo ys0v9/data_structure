7c2d5361cf6f5d65bfbb21edf5c2d1702a18f999
tndk <sua9097@gmail.com>
2025-09-01 11:43:31 +0900

    공백 상태 검사 isEmpty()

--- arr_list.py
+++ arr_list.py
@@ -3,3 +3,11 @@
 capacity = 100 # 리스트 용량: 예) 용량을 100으로 지정
 array = [None] * capacity # 요소 배열: [None, .., None] (길이가 capacity)
 size = 0
+
+# 공백 상태 검사 isEmpty()
+def isEmpty():
+    if size == 0:
+        return True # 공백이면 True 반환
+    else:
+        return False # 아니면 False 반환
+    
