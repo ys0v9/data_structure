70dbdc90be3e8d970a95e1079956d4a5772b5234
tndk <sua9097@gmail.com>
2025-09-17 14:10:38 +0900

    evalPostfix

--- stack/ArrayStack.py
+++ stack/ArrayStack.py
@@ -1,37 +1,38 @@
 class ArrayStack:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.array = [None]*self.capacity
+        self.array = [None] * self.capacity
         self.top = -1
 
     def isEmpty(self):
         return self.top == -1
     def isFull(self):
-        return self.top == self.capacity-1
+        return self.top == self.capacity - 1
     
     def push(self, item):
         if not self.isFull():
-            self.top +=1
+            self.top += 1
             self.array[self.top] = item
         else:
-            print("stack overflow")
+            raise IndexError("stack overflow")
 
     def pop(self):
-        if not self.isEmpty:
+        if not self.isEmpty():
             item = self.array[self.top]
             self.top -= 1
             return item
         else:
-            print("stack overflow")
+            raise IndexError("stack underflow")
     
     def peek(self):
         if not self.isEmpty():
             return self.array[self.top]
         else:
-            print("stack underflow")
+            raise IndexError("stack underflow")
     
     def __str__(self):
         return str(self.array[0:self.top+1])
+
 
 
 
