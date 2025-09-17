98941dd0fa3fcd912a78edfc784ee321c60e3e5e
tndk <sua9097@gmail.com>
2025-09-17 14:01:29 +0900

    checkBrackets

--- stack/ArrayStack.py
+++ stack/ArrayStack.py
@@ -45,3 +45,28 @@ if __name__ == "__main__":
     print(' pop 1회: ', s)
 
     print(s.peek())
+
+
+
+def checkBrackets(statement):
+    stack = ArrayStack(100)
+    for ch in statement:
+        if ch == '{' or ch == '[' or ch == '(':
+            stack.push(ch)
+        elif ch == '}' or ch == ']' or ch == ')':
+            if stack.isEmpty():
+                return False
+            else:
+                left = stack.pop()
+                if (ch == "}" and left != "{") or (ch == "]" and left != "[") or (ch ==")" and left != "("):
+                    return False
+                
+    return stack.isEmpty()
+
+# Test
+s1 = "{ A[(i+1)] = 0;}"
+s2 = "if( (i==0) && (j==0)"
+s3 = "A[ (i+1]) = 0;"
+print(s1, " ---> ", checkBrackets(s1))
+print(s2, " ---> ", checkBrackets(s2))
+print(s3, " ---> ", checkBrackets(s3))
