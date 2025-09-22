264cfde8c02b46b2a3cf7abe8743d435e685ce3e
tndk <sua9097@gmail.com>
2025-09-22 14:22:44 +0900

    Test

--- stack/Infix2Postfix.py
+++ stack/Infix2Postfix.py
@@ -38,3 +38,26 @@ def Infix2Postfix(expr):
     while not s.isEmpty():
         output.append(s.pop())
     return output
+
+
+
+# Test
+if __name__ == "__main__":
+    print('중위 표기식 후위 표기 변환₩n')
+
+    infix1 = ['8','/','2','-','3','+','(','3','*','2',')']
+    infix2 = ['1','/','2','*','4','*','(','1','/','4',')']
+
+    postfix1 = Infix2Postfix(infix1)
+    postfix2 = Infix2Postfix(infix2)
+
+    result1 = evalPostfix(postfix1)
+    result2 = evalPostfix(postfix2)
+
+    print(' 중위표기:', infix1)
+    print(' 후위표기', postfix1)
+    print(' 게산결과:', result1, end ='₩n₩n')
+
+    print(' 중위표기:', infix2)
+    print(' 후위표기', postfix2)
+    print(' 게산결과:', result2)
